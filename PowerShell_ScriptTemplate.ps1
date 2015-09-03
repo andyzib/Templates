48f572048c71108464795d9fc38bb2c39bad2a3f
Andrew Zbikowski <andrew@zibnet.us>
2015-09-03 15:02:45 -0500

    Added parameter sections with some examples to my PowerShell template.

--- PowerShell_ScriptTemplate.ps1
+++ PowerShell_ScriptTemplate.ps1
@@ -25,11 +25,51 @@ Purpose/Change: Initial script development
 <Example goes here. Repeat this attribute for more than one example>
 #>
  
+#-------------[Parameters]-----------------------------------------------------
+# Enable -Debug, -Verbose Parameters. Write-Debug and Write-Verbose!
+#[CmdletBinding()]
+ 
+<#
+Param (
+    [Parameter(Mandatory=$true)][string]$csv = $( Read-Host "Enter path and filename to CSV file"),
+    [Parameter()][string]$outdir = "$($env:USERPROFILE)\Documents",
+    [Parameter()][string]$outstring = "Example"
+)
+#>
+ 
+#-------------[Parameter Validation]-------------------------------------------
+# Sanitize User Input
+ 
+<#
+# Check that the CSV file exists.
+$csv = $csv.Trim()
+if (-Not (Test-Path -PathType Leaf -Path $csv)) {
+    Throw "CSV not found: $csv"
+}
+#>
+ 
+<#
+# Strip trailing \ from $outdir
+$outdir = $outdir.Trim()
+$outdir = $outdir.TrimEnd("\")
+# Check that outdir exists.
+if (-Not (Test-Path -PathType Container -Path $outdir)) {
+    Throw "Output directory note found: $outdir"
+}
+#>
+ 
+<#
+# Strip illegal file name characters with a RegEx.
+# https://gallery.technet.microsoft.com/scriptcenter/Remove-Invalid-Characters-39fa17b1
+$outstring = [RegEx]::Replace($outstring, "[{0}]" -f ([RegEx]::Escape([String][System.IO.Path]::GetInvalidFileNameChars())), '')
+#>
+ 
 #-------------[Initialisations]------------------------------------------------
  
 #Set Error Action to Silently Continue
 #$ErrorActionPreference = "SilentlyContinue"
  
+#Import-Module activedirectory -ErrorAction Stop
  
 #-------------[Declarations]---------------------------------------------------
  
@@ -40,6 +80,13 @@ Purpose/Change: Initial script development
 #$sLogPath = "C:\Windows\Temp"
 #$sLogName = "<script_name>.log"
 #$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+ 
+# ISO 8601 Date Format. Accept no substuties! 
+$iso8601 = Get-Date -Format s
+# Colon (:) isn't a valid character in file names.
+$iso8601 = $iso8601.Replace(":","_")
+# Just YYYY-MM-DD
+#$datestamp = $iso8601.Substring(0,10)
  
 #-------------[Functions]------------------------------------------------------
  
